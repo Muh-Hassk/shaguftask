38bf4735fe99f6fb45f17bc94020013a33e6a019
MuhammedHassan <Moh.alkhuzaee@gmail.com>
2024-02-07 14:53:24 +0300

    s

--- shaguftask/settings.py
+++ shaguftask/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ShagufTask',
         'USER': 'root',
-        'PASSWORD': '3124ahlaMNAM',
+        'PASSWORD': 'password',
         'HOST': 'localhost',  # or the hostname where your MySQL server is running
         'PORT': '3306',  # or the port on which your MySQL server is listening
 
